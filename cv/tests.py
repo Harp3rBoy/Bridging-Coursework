c1bcb90fe0399896d3b2dedcbba2223da1bfb442
Ben Harper <ben27.harper@gmail.com>
2020-08-20 16:06:34 +0100

    First unit test and url mapping, dummy view

--- cv/tests.py
+++ cv/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from cv.views import home_page
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_cv_url_resolves_to_home_page_view(self):
+        found = resolve('/cv')
+        self.assertEqual(found.func, home_page)
