98b329bf08bfd88f4f00a40c9513ee85ef8e1e58
Harp3rBoy <ben27.harper@gmail.com>
2020-08-23 00:16:01 +0100

    Removed (time.sleep)s from FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 import time
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_view_cv_page(self):
         self.browser.get(self.live_server_url + '/cv')
@@ -35,17 +45,15 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Ben Harper')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('Ben Harper')
+        self.wait_for_row_in_list_table('Ben Harper')
 
         # dob
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('27/06/2000')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('Ben Harper')
-        self.check_for_row_in_list_table('27/06/2000')
+        self.wait_for_row_in_list_table('Ben Harper')
+        self.wait_for_row_in_list_table('27/06/2000')
 
         self.fail('Finish the test!')
 
