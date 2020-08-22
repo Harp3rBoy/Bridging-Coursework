c023803337c2dac15a283302aeb978afd8df36f0
Harp3rBoy <ben27.harper@gmail.com>
2020-08-22 17:47:16 +0100

    Expanded functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,8 +15,28 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_view_cv_page(self):
         self.browser.get('http://localhost:8000/cv')
         self.assertIn('CV', self.browser.title)
+
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('CV', header_text)
+
+        # personal details div
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter your name'
+        )
+
+        inputbox.send_keys('Ben Harper')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Ben Harper' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 
