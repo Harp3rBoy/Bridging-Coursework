ba070798d8462610ca7af3b5bb0094ddc45c7d40
Harp3rBoy <ben27.harper@gmail.com>
2020-08-29 15:15:00 +0100

    Edited FT to remove redundant function and add in new wait_for function instead of 'time.sleep()'s

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,6 +7,17 @@ import time
 MAX_WAIT = 10
 
 
+def wait_for(fn):
+    start_time = time.time()
+    while True:
+        try:
+            return fn()
+        except (AssertionError, WebDriverException) as e:
+            if time.time() - start_time > MAX_WAIT:
+                raise e
+            time.sleep(0.5)
+
+
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -14,19 +25,6 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
 
     def test_can_view_cv_page(self):
         self.browser.get(self.live_server_url + '/cv/')
@@ -42,10 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
         edit_details = self.browser.find_element_by_id('id_edit_details')
         edit_details.click()
 
-        time.sleep(1)
-
-        header_text = self.browser.find_element_by_tag_name('h2').text
-        self.assertIn('Edit Personal Details', header_text)
+        # wait for new view
+        wait_for(lambda: self.assertIn("Edit Personal Details", self.browser.find_element_by_tag_name('h2').text))
 
         edit_form = self.browser.find_element_by_tag_name('form')
 
@@ -62,7 +58,7 @@ class NewVisitorTest(LiveServerTestCase):
         save = edit_form.find_element_by_tag_name('button')
         save.click()
 
-        time.sleep(1)
+        wait_for(lambda: self.assertIn("CV", self.browser.find_element_by_tag_name('h1').text))
 
         name = self.browser.find_element_by_id('name').text
         self.assertEqual(name, 'Ben Harper')
@@ -72,6 +68,17 @@ class NewVisitorTest(LiveServerTestCase):
 
         email = self.browser.find_element_by_id('email').text
         self.assertEqual(email, 'blh898@student.bham.ac.uk')
+
+        # education div
+        education = self.browser.find_element_by_id('id_education').text
+        self.assertIn('Education', education)
+
+        add_education = self.browser.find_element_by_id('id_add_education')
+        add_education.click()
+
+        wait_for(lambda: self.assertIn("Edit Education", self.browser.find_element_by_tag_name('h2').text))
+
+        edit_form = self.browser.find_element_by_tag_name('form')
 
         self.fail('Finish the test!')
 
