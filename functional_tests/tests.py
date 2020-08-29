280826436a085afe7d865e7ae740f220afe7c67c
Harp3rBoy <ben27.harper@gmail.com>
2020-08-29 15:33:19 +0100

    Expanded FT for testing of education

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -26,13 +26,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_view_cv_page(self):
+    def go_to_cv_page(self):
         self.browser.get(self.live_server_url + '/cv/')
+
+    def test_can_view_cv_page(self):
+        self.go_to_cv_page()
         self.assertIn('CV', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('CV', header_text)
 
+    def test_can_edit_personal_details(self):
+        self.go_to_cv_page()
         # personal details div
         personal_details = self.browser.find_element_by_id('id_personal_details').text
         self.assertIn('Personal Details', personal_details)
@@ -69,6 +74,8 @@ class NewVisitorTest(LiveServerTestCase):
         email = self.browser.find_element_by_id('email').text
         self.assertEqual(email, 'blh898@student.bham.ac.uk')
 
+    def test_can_add_education(self):
+        self.go_to_cv_page()
         # education div
         education = self.browser.find_element_by_id('id_education').text
         self.assertIn('Education', education)
@@ -80,6 +87,33 @@ class NewVisitorTest(LiveServerTestCase):
 
         edit_form = self.browser.find_element_by_tag_name('form')
 
-        self.fail('Finish the test!')
+        inputbox = edit_form.find_element_by_name('institution')
+        inputbox.send_keys('My School')
 
+        inputbox = edit_form.find_element_by_name('grades')
+        inputbox.send_keys('Maths - A')
 
+        inputbox = edit_form.find_element_by_name('start_date')
+        inputbox.clear()
+        inputbox.send_keys('01/09/2016')
+
+        inputbox = edit_form.find_element_by_name('end_date')
+        inputbox.clear()
+        inputbox.send_keys('01/07/2018')
+
+        save = edit_form.find_element_by_tag_name('button')
+        save.click()
+
+        wait_for(lambda: self.assertIn("CV", self.browser.find_element_by_tag_name('h1').text))
+
+        institution = self.browser.find_element_by_id('institution').text
+        self.assertEqual(institution, 'My School')
+
+        grades = self.browser.find_element_by_id('grades').text
+        self.assertEqual(grades, 'Maths - A')
+
+        start_date = self.browser.find_element_by_id('start_date').text
+        self.assertEqual(start_date, '1 Sept 2016')
+
+        end_date = self.browser.find_element_by_id('end_date').text
+        self.assertEqual(end_date, '1 July 2018')
